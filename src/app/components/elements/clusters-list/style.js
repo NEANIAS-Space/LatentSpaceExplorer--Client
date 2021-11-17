ecf0489e662224fe02eed9f0c4b01d5b10783c3f
Lucas Puerari <lucas.puerari@unimib.it>
2021-11-17 17:07:37 +0100

    Cluster list update scroll type

--- src/app/components/elements/clusters-list/style.js
+++ src/app/components/elements/clusters-list/style.js
@@ -4,7 +4,7 @@ const ClusterListWrapper = styled.div`
     position: relative;
     width: 100%;
     height: 200px;
-    overflow: scroll;
+    overflow-y: scroll;
 `;
 
 export default ClusterListWrapper;
