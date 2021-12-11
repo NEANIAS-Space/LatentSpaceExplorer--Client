221a6bfc2dc08705acc1c758cd2ff59e4b52a98d
Lucas Puerari <lucas.puerari@unimib.it>
2021-12-11 11:28:41 +0100

    Refactor projector layout

--- src/app/components/layouts/projector-layout/index.jsx
+++ src/app/components/layouts/projector-layout/index.jsx
@@ -3,14 +3,14 @@ import Header from 'app/components/modules/header';
 import ProjectorLayoutWrapper from './style';
 
 const ProjectorLayout = ({ children }) => (
-  <ProjectorLayoutWrapper>
-    <Header />
-    {children}
-  </ProjectorLayoutWrapper>
+    <ProjectorLayoutWrapper>
+        <Header />
+        {children}
+    </ProjectorLayoutWrapper>
 );
 
 ProjectorLayout.propTypes = {
-  children: PropTypes.element.isRequired,
+    children: PropTypes.element.isRequired,
 };
 
 export default ProjectorLayout;
