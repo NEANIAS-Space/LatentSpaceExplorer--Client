9a8c87db9ea86070b0dccf3174633deff12e6b13
Lucas Puerari <lucas.puerari@unimib.it>
2021-12-14 16:44:14 +0100

    Update template projector

--- src/app/components/templates/projector/index.js
+++ src/app/components/templates/projector/index.js
@@ -1,29 +1,73 @@
-import React from 'react';
+import { useState } from 'react';
 import ProjectorLayout from 'app/components/layouts/projector-layout';
+import ProjectorContext from 'app/contexts/projector';
 import SideBar from 'app/components/modules/sidebar';
 import PrimaryContent from 'app/components/modules/primary-content';
 import VisualizationForm from 'app/components/modules/forms/visualization';
-// import Graph from 'app/components/elements/graph';
-// import ProjectorContext from 'app/contexts/projector/context';
-// import ReductionForm from 'app/components/modules/forms/reduction';
-// import ClusterForm from 'app/components/modules/forms/cluster';
-// import PreviewImage from 'app/components/elements/preview-image';
-// import ClustersList from 'app/components/elements/clusters-list';
+import ReductionForm from 'app/components/modules/forms/reduction';
+import MessageBox from 'app/components/elements/message-box';
+import Graph from 'app/components/elements/graph';
+import PreviewImage from 'app/components/elements/preview-image';
 
-const ProjectorTemplate = () => (
-    <ProjectorLayout>
-        <>
-            <SideBar column={1}>
-                <VisualizationForm />
-            </SideBar>
-            <PrimaryContent>
-                <>ciao</>
-            </PrimaryContent>
-            <SideBar column={3}>
-                <>ciao</>
-            </SideBar>
-        </>
-    </ProjectorLayout>
-);
+const ProjectorTemplate = () => {
+    const [openMessageBox, setOpenMessageBox] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const [graphData, setGraphData] = useState([]);
+
+    const [previewImage, setPreviewImage] = useState('');
+
+    const handleCloseMessageBox = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenMessageBox(false);
+    };
+
+    const renderMessageBox = () => (
+        <MessageBox
+            message={errorMessage}
+            severity="error"
+            open={openMessageBox}
+            handleClose={handleCloseMessageBox}
+        />
+    );
+
+    return (
+        <ProjectorLayout>
+            <ProjectorContext.Provider
+                value={{
+                    openMessageBox,
+                    setOpenMessageBox,
+                    errorMessage,
+                    setErrorMessage,
+                    graphData,
+                    setGraphData,
+                    previewImage,
+                    setPreviewImage,
+                }}
+            >
+                <SideBar column={1}>
+                    <>
+                        <VisualizationForm />
+                        <ReductionForm />
+                    </>
+                </SideBar>
+                <PrimaryContent>
+                    <Graph />
+                </PrimaryContent>
+                <SideBar column={3}>
+                    <>
+                        {previewImage && (
+                            <PreviewImage imageName={previewImage} />
+                        )}
+                    </>
+                </SideBar>
+                {renderMessageBox()}
+            </ProjectorContext.Provider>
+        </ProjectorLayout>
+    );
+};
 
 export default ProjectorTemplate;
