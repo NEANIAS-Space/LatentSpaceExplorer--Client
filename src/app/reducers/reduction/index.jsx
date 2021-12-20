bc808a361869c04106e9d81158492fa75da62c3c
Lucas Puerari <lucas.puerari@unimib.it>
2021-12-20 10:33:46 +0100

    Add reduction form reducer

---
+++ src/app/reducers/reduction/index.jsx
@@ -0,0 +1,23 @@
+const reductionFormReducer = (state, action) => {
+    switch (action.type) {
+        case 'COMMON':
+            return {
+                ...state,
+                [action.field]: action.value,
+            };
+
+        case 'ALGORITHM':
+            return {
+                ...state,
+                [action.algorithm]: {
+                    ...state[action.algorithm],
+                    [action.field]: action.value,
+                },
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default reductionFormReducer;
