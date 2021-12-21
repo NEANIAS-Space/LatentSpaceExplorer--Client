a698394bd964f4978d7aa6e2fbf63b16a02cbd42
Lucas Puerari <lucas.puerari@unimib.it>
2021-12-21 14:05:50 +0100

    Remove camelCaseKeysTounderscore function

--- src/app/utils/formatter.jsx
+++ src/app/utils/formatter.jsx
@@ -1,10 +1,3 @@
-const camelCaseKeysToUnderscore = (o) =>
-    Object.keys(o).reduce((acc, k) => {
-        const newKey = k.replace(/[A-Z]/g, (m) => `_${m.toLowerCase()}`);
-        acc[newKey] = o[k];
-        return acc;
-    }, {});
-
 const normalize = (str) => String(str).toLowerCase().replaceAll('_', ' ');
 
-export { camelCaseKeysToUnderscore, normalize };
+export { normalize };
